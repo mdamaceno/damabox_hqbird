4629efa2bb3c8a8b3b6f91686f9db72324e0ef9a
Marco Damaceno <maadamaceno@gmail.com>
2017-09-20 12:28:39 -0300

    Changed start process

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-service hqbird start
-service firebird start
+# service hqbird start
 
-tail -n 100 /var/log/dataguard32.log
-tail -f /var/log/dataguard32.log
+/opt/firebird/bin/fb_smp_server &
+
+service hqbird console
